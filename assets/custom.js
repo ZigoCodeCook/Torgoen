600a63f74dcb8322c0d0eb1545066cdab70033d1
ZigoCodeCook <ziv@codecook.com>
2024-08-19 13:14:21 +0300

    strap engine test

--- assets/custom.js
+++ assets/custom.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // TOGGLE SWITCH JS START
 
     // Add event listener to toggle dark mode
-    document.getElementById('modeToggle').addEventListener('change', function() {
+    document.getElementById('modeToggle')?.addEventListener('change', function() {
         const body = document.body;
         body.classList.toggle('dark-mode');
         localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
@@ -127,4 +127,208 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+
+
+
+    // STRAP ENGINE OBJECT
+    const strapEngine = (function(){
+
+        const params = {
+            data : null,
+            jsonRowData: null,
+            jsonData: [],
+            selectedCollection: '',
+            selectedModel: '',
+        }
+
+        const int = () => {
+            
+            console.log('strap init');
+
+            // testing result 
+            testingCode();
+
+            // add output contianer
+            document.body.insertAdjacentHTML('beforeend', '<div class="strapenginescreen"><div class="strapengine"></div></div>');
+           
+            readData();
+        }
+
+
+
+        const testingCode = () => {
+
+            const styles = `
+            .strapenginescreen {
+               position: absolute;
+               top: 10em;
+               left: 50%;
+               transform: translateX(-50%);
+               background-color: #fff;
+               border: 1px solid;
+               z-index: 999;
+               padding: 2em;
+               }
+           `;             
+
+           document.body.insertAdjacentHTML('beforeend', `<style>${styles}</style>`); 
+
+        }
+
+
+        const readData = () => {
+
+            const sheetUrl = 'https://docs.google.com/spreadsheets/d/1-x59uEuFZVtK-tZoUSn4koPptZ2YAiFmLeskw9eA4E4/pub?gid=0&single=true&output=csv';
+
+            fetch(sheetUrl)
+                .then(response => response.text())
+                .then(csv => {
+                    // Convert CSV to JSON
+                    const json = csvToJson(csv);
+
+                    params.data = csv;
+                    params.jsonRowData = json;
+                    params.jsonData = groupByIdentifier(params.jsonRowData);
+
+                    //console.log(params.jsonData);
+
+                    buildFirstSelect();
+                    buildSecondSelect();
+                    buildThirdSelect();
+
+                })
+                .catch(error => console.error('Error fetching data:', error));
+
+        }
+
+        // build collection select (1st)
+        const buildFirstSelect = () => {
+
+            const colSelect = document.createElement('select');
+            for (let identifier in params.jsonData){
+                let colOption = document.createElement('option');
+                colOption.value = params.jsonData[identifier].Identifier;
+                colOption.text = params.jsonData[identifier].Collection;
+                colSelect.appendChild(colOption);
+            }
+
+            document.querySelectorAll('.strapengine')[0].appendChild(wrapInDiv(colSelect));
+            params.selectedCollection = colSelect.value;
+
+            colSelect.addEventListener('change',(evt)=>{
+                params.selectedCollection = colSelect.value;
+                document.querySelectorAll('.modelSelect')[0].remove();  // remove model select and rebuild
+                document.querySelectorAll('.strapSelect')[0].remove();  // remove strap select and rebuild
+                buildSecondSelect();    
+                buildThirdSelect();                                 
+            });
+
+        }
+
+        // build model select (2nd)
+        const buildSecondSelect = () => {
+
+            const colSelect = document.createElement('select');
+            const models = params.jsonData[params.selectedCollection].Models;
+            colSelect.setAttribute('class', 'modelSelect');
+
+
+            for (let identifier in models){
+                let colOption = document.createElement('option');
+                let name = models[identifier]['Model Name'];
+                colOption.value = name;
+                colOption.text = name;
+                colSelect.appendChild(colOption);
+            }
+
+            document.querySelectorAll('.strapengine')[0].appendChild(wrapInDiv(colSelect));
+            params.selectedModel = colSelect.value;
+
+            colSelect.addEventListener('change',(evt)=>{
+                params.selectedModel = colSelect.value;
+                document.querySelectorAll('.strapSelect')[0].remove();  // remove strap select and rebuild
+                buildThirdSelect();                                    // build strap select
+            });
+
+        }
+
+        // build model select (3rd)
+        const buildThirdSelect = () => {
+            const colSelect = document.createElement('select');
+            const models = params.jsonData[params.selectedCollection].Models;
+            const strapCollection = models[params.selectedModel]?.['StrapCollection'];
+            colSelect.setAttribute('class', 'strapSelect');
+
+            const option = document.createElement('option');
+            option.value = strapCollection;
+            option.text  = strapCollection;
+            colSelect.appendChild(option);
+            document.querySelectorAll('.strapengine')[0].appendChild(wrapInDiv(colSelect));
+
+        }
+
+        const wrapInDiv = (obj) => {
+            const div = document.createElement('div');
+            div.appendChild(obj);
+            return div;
+        }
+
+        const groupByIdentifier = (data) => {
+            return data.reduce((result, item) => {
+                const id = item['Identifier'];
+                const discontinuedCollection = item['Discontinued collection'] || 'False';
+                const collection = item['Collection name'];
+                const modelName = item['Model name'];
+                const discontinuedModel = item ['Discontinued model'];
+
+                // Initialize the identifier entry if it doesn't exist
+                if (!result[id]) {
+                    result[id] = { Identifier: id, Collection: collection, dicontinued: discontinuedCollection ,Models: {} };
+                }
+        
+                // Initialize the model entry if it doesn't exist
+                if (modelName && !result[id].Models[modelName]) {
+                    result[id].Models[modelName] = {
+                        'Model Name': modelName,
+                        'Discontinued' : discontinuedModel || 'False',
+                        'Image': item['Image'] || '',
+                        'Lugs Width': item['Lugs width'] || '',  
+                        'Model Size': item['Model size'] || '',
+                        'StrapCollection': item['Strap collection'] || ''
+                    };
+                }
+
+                return result;
+            }, {});
+        }
+
+        /**
+         * Convert CSV to JSON
+         * @param {string} csv - CSV data as a string
+         * @returns {Object[]} - Array of JSON objects
+         */
+        const csvToJson = (csv) => {
+            const lines = csv.split('\n');
+            const headers = lines[0].split(',');
+
+            return lines.slice(1).map(line => {
+                const values = line.split(',');
+                return headers.reduce((obj, header, i) => {
+                    obj[header.trim()] = values[i].trim();
+                    return obj;
+                }, {});
+            });
+        }
+
+
+        return {
+            int:int
+        }
+
+
+    })();
+
+    strapEngine.int();
+
+
 });
